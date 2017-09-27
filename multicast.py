0bbc7ca390c11be892bf5524e3587755b133054c
ulyssejdv <u.devillartay@gmail.com>
2017-09-27 16:36:28 +0200

    Update multicast.py

--- multicast.py
+++ multicast.py
@@ -7,8 +7,9 @@ class Multicast(Thread):
     MCAST_GRP = "224.1.1.1"
     MCAST_PRT = 5007
 
-    def __init__(self):
+    def __init__(self, srv_q):
         Thread.__init__(self)
+        self.srv_q = srv_q
 
     def run(self):
         self.receive()
@@ -20,9 +21,11 @@ class Multicast(Thread):
         mreq = struct.pack("4sl", socket.inet_aton(self.MCAST_GRP), socket.INADDR_ANY)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
         while True:
-            print("{0} join the group !".format(sock.recv(10240)))
+            new_srv = sock.recv(10240)
+            print("{0} join the group !".format(new_srv))
+            self.srv_q.put(new_srv)
 
-    def send(self, name):
+    def send(self, srv):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-        sock.sendto(str.encode(name), (self.MCAST_GRP, self.MCAST_PRT))
+        sock.sendto(str.encode(srv), (self.MCAST_GRP, self.MCAST_PRT))
